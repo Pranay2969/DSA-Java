a8ecd67b2646b25673041e3b9b3ba81b87ad094d
Pranay1108 <mpranaykumarreddy781@gmail.com>
2026-03-11 23:22:18 +0530

    Add PalindromeNumber.java to check if an integer is a palindrome

---
+++ Math/PalindromeNumber.java
@@ -0,0 +1,29 @@
+package Math;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+  public static boolean isPalindrome(int n) {
+    if(n < 0 || n % 10 == 0) return false;
+    int rev = 0;
+    int dup = n;
+
+    while(n != 0) {
+      rev = rev * 10 + n % 10;
+      n /= 10;
+    }
+
+    return rev == dup;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    boolean flag = isPalindrome(n);
+    System.out.println(flag);
+    
+    sc.close();
+  }
+}
