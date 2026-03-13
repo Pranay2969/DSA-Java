65c5ccabad1449dce7614e6c7ec7fe3935976621
Pranay <mpranaykumarreddy781@gmail.com>
2026-03-13 19:16:21 +0530

    Add Factorial.java to calculate the factorial of a given integer

---
+++ Math/Factorial.java
@@ -0,0 +1,21 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Factorial {
+  public static int fact(int n) {
+    if(n <= 1)
+      return 1;
+    return n * fact(n - 1);
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    int factorial = fact(n);
+    System.out.println(factorial);
+    
+    sc.close();
+  }
+}
